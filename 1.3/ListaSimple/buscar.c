11bb89034b680e8d118909e51e706aaec29691fc
GustavoVaAsc <quickgohan98@gmail.com>
2025-04-12 23:16:31 -0600

    Comento las funciones buscar e inicializarLista (Lista Simple)

--- 1.3/ListaSimple/buscar.c
+++ 1.3/ListaSimple/buscar.c
@@ -1,10 +1,15 @@
-Nodo buscarEnLista(Lista *lista, char *nombre){
-    if(lista->primero == NULL) return NULL;
+// Función para buscar en una lista, en este caso por nombre
+
+Nodo buscarEnLista(Lista *lista, char *nombre){ 
+    if(lista->primero == NULL) return NULL; // Si la lista está vacía, devolvemos NULL
     
-    Nodo *iterador = lista->primero;
+    Nodo *iterador = lista->primero; // Apuntador para iterar la lista
+
+    // Mientras no lleguemos a NULL
     while(iterador!=NULL){
-        if(strcmp(iterador->nombre, nombre) == 0) return iterador;
-        iterador = iterador->siguiente;
+        // Comparamos las cadenas con la funcion strcmp de string.h
+        if(strcmp(iterador->nombre, nombre) == 0) return iterador; // Si son iguales, devolvemos el nodo
+        iterador = iterador->siguiente; // Pasamos al siguiente
     }
-    return NULL;
+    return NULL; // Si no lo encontramos devolvemos NULL
 }
--- 1.3/ListaSimple/inicializarLista.c
+++ 1.3/ListaSimple/inicializarLista.c
@@ -1,4 +1,6 @@
+// Función para inicializar la lista
+
 void inicializarLista(Lista *lista){
-    lista->primero = NULL;
-    lista->tam = 0;
+    lista->primero = NULL; // El primer elemento es NULl
+    lista->tam = 0; // Su tamaño es 0
 }
