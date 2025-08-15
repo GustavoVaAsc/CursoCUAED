251628e53c28d2548e8e9bc4ae535834c2e7e3d0
GustavoVaAsc <quickgohan98@gmail.com>
2025-08-14 18:09:57 -0600

    Agrego la actividad3 del tema apuntadores

---
+++ 1.1/Apuntadores/actividad3.c
@@ -0,0 +1,40 @@
+/*
+
+Escribe un programa en lenguaje C que sume 1 a todos los
+elementos impares de un arreglo usando apuntadores.
+
+Indicación importante: NO USAR LOS ÍNDICES DEL ARREGLO.
+
+Completa el código con las siguientes opciones:
+
+1. scanf("%lf",&a[i]);
+2. sizeof(arr)/sizeof(arr[0]);
+3. 
+4. arr
+5. arr
+
+*/
+
+#include <stdio.h>
+
+int main(){
+    int arr[] = {1,2,4,5,6,18,23,25,28,30};
+    int tam = sizeof(arr)/sizeof(arr[0]); // 2
+    int *p = arr; // 5
+
+    int *final = arr + tam;
+
+    while(p < final){
+        if((*p)%2 == 1){ // 4
+            (*p)++; // 3
+        }
+        p++; // 1
+    }
+
+    for(int i=0; i<10; i++){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
--- 1.1/Arreglos/actividad4.c
+++ 1.1/Arreglos/actividad4.c
@@ -7,7 +7,7 @@ selecciona la instrucción que arregla el programa
 a) int transpuesta[N][N]; -> int transpuesta[N][N] = matriz;
 b) transpuesta[i][i] = matriz[j][j]; -> transpuesta[i][j] = matriz[i][j];
 c) scanf("%d",&matriz[i][j]); -> scanf("%d",matriz[i][j]);
-d) b) transpuesta[i][i] = matriz[j][j]; -> transpuesta[i][j] = matriz[j][i]; (Correcta)
+d) transpuesta[i][i] = matriz[j][j]; -> transpuesta[i][j] = matriz[j][i]; (Correcta)
 
 Retroalimentación A: Respuesta incorrecta: No se puede inicializar de esa forma la matriz
 Retroalimentación B: Respuesta incorrecta: Solo se está copiando la matriz
@@ -15,6 +15,8 @@ Retroalimentación C: Respuesta incorrecta: Debemos usar el & para escribir el d
 Retroalimentación D: Respuesta correcta
 
 */
+
+
 
 #include <stdio.h>
 
